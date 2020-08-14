b038f5809d3f9b7405fe44dbce42a6297aa531b5
Mohammad Reza Kardoost <mrk74@outlook.com>
2020-08-14 22:28:35 +0430

    removed traditional splash screen and added a new one

--- app/src/main/java/com/github/rezakardoost/bourshub/ui/HomeFragment.kt
+++ app/src/main/java/com/github/rezakardoost/bourshub/ui/HomeFragment.kt
@@ -7,15 +7,13 @@ import android.view.View
 import android.view.ViewGroup
 import com.github.rezakardoost.bourshub.R
 
-
-class SplashScreenFragment : Fragment() {
+class HomeFragment : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_splash_screen, container, false)
+        return inflater.inflate(R.layout.fragment_home, container, false)
     }
-
 }
--- app/src/main/java/com/github/rezakardoost/bourshub/ui/MainActivity.kt
+++ app/src/main/java/com/github/rezakardoost/bourshub/ui/MainActivity.kt
@@ -5,11 +5,11 @@ import android.content.res.Configuration
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
-import android.widget.Toast
 import com.github.rezakardoost.bourshub.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        setTheme(R.style.AppTheme)
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
